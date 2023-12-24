63d26391e0af068cb3aa71dbfd3a610ee59e8748
lfigueroaz <lfigueroaz@bancosol.com.bo>
2023-12-23 23:26:27 -0400

    Added fields

--- WebApiMock/Controllers/Account/GetMovementsData.cs
+++ WebApiMock/Controllers/Account/GetMovementsData.cs
@@ -14,6 +14,14 @@ public class TransactionDescription
     public string Text { get; set; }
 }
 
+public class TransactionType 
+{ 
+    public int ModuleCode { get; set; }
+    public int TransactionNumberCode { get; set; }
+    public int OrdinalCode { get; set; }
+    public string Description { get; set; }
+}
+
 public class TransactionData
 {
     public string TransactionReferenceId { get; set; } = "10102023/295/400/200/1522";
@@ -24,6 +32,8 @@ public class TransactionData
     public DateTime TransactionDate { get; set; }
     public string TransactionChannelCode { get; set; } = "3";
     public string TransactionBranchCode { get; set; } = "295";
+    public TransactionDescription TransactionBeneficiary { get; set; }
+    public TransactionType TransactionType { get; set; }
     public TransactionCode TransactionCode { get; set; } = new TransactionCode { Code = 1522, GroupCode = 400200 };
     public TransactionDescription TransactionDescription { get; set; } = new TransactionDescription { Text = "Pago QR ACH"};
     public TransactionDescription TransactionClientNote { get; set; } = new TransactionDescription
